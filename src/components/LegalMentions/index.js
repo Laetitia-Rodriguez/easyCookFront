8fd257ca31395e3bf997d951c5662c5a18ad94ff
LaetitiaRodriguez <laetirodriguez@hotmail.com>
2021-06-30 11:20:10 +0200

    Feat: add LegalMentions Route and component to test react-router : ok

---
+++ src/components/LegalMentions/index.js
@@ -0,0 +1,10 @@
+// Local import
+import './legalMentions.css';
+
+const LegalMentions = () => (
+    <div className="legal-mentions">
+        Page mentions légales
+    </div>
+)
+
+export default LegalMentions;
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -3,12 +3,16 @@ import { Switch, Route } from 'react-router-dom';
 
 // Local import
 import Homepage from '../../components/Homepage';
+import LegalMentions from '../../components/LegalMentions';
 import './main.css';
 
 const Main = () => (
     <main className="main">
         <Switch>
-            <Route path="/">
+            <Route path="/mentions-légales" exact>
+                <LegalMentions />
+            </Route>
+            <Route path="/" exact>
                 <Homepage />
             </Route>
         </Switch>
