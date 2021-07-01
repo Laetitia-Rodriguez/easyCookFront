7d7f4327e749eb33d275bca0a23e34a078fd32a4
LaetitiaRodriguez <laetirodriguez@hotmail.com>
2021-07-01 15:58:19 +0200

    Style:fix css Homepage (header...)

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,7 @@
 // Import images
 import headerImage from '../../assets/headerImage.jpg';
 import headerLogo from '../../assets/headerLogo.png';
-import egg from '../../assets/egg.png';
+import LogoEasyCook from '../../assets/LogoEasyCook.png';
 
 // Import css
 import './header.css';
@@ -9,17 +9,10 @@ import './header.css';
 
 const Header = () => (
     <header className="header">
-        <div className="header__title">
-        {/* TODO: Make EasyCook logo with title and eggs on a graphics software
-            Like this : SEO problem with "EasyCook" word and css "homemade" */}
-            <h1 className="header__h1_left">EasyC</h1>
-            <img src={egg} className="header__egg" alt=""/>
-            <img src={egg} className="header__egg" alt=""/>
-            <h1 className="header__h1_right">k</h1>
-            <a href="/">
-                <img src={headerLogo} className="header__logo" alt="Chef Gear Hat icon by Streamline"/>
-            </a>
-        </div>
+        <a href="/" className="header__title">
+            <img src={LogoEasyCook} className="header__EasyCook" alt=""/>
+            <img src={headerLogo} className="header__logo" alt="Chef Gear Hat icon by Streamline"/>
+        </a>
         <div className="header__main">
             <p className="header__tagline">
                 Une application simple pour cuisiner avec ce qu'il y a dans le frigo.
