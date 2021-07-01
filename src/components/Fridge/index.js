4a3f49216910a0573d89e11d719afd3cb3a36f22
LaetitiaRodriguez <laetirodriguez@hotmail.com>
2021-07-01 21:47:07 +0200

    Feat:static integration Fridge

--- src/components/Fridge/index.js
+++ src/components/Fridge/index.js
@@ -3,7 +3,12 @@ import './fridge.css';
 
 const Fridge = () => (
     <div className="fridge">
-        page mon frigo
+        <h2>Dans mon frigo (et mon placard) j'ai:</h2>
+        <ul>
+            <li>Groupes</li>
+            <li>Sous-groupes</li>
+        </ul>
+        <input type="submit" value="Chercher des recettes"/>
     </div>
 )
 
