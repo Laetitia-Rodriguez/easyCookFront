81122341d3abea51f08e68f4d623ad0aa630d58e
LaetitiaRodriguez <laetirodriguez@hotmail.com>
2021-07-01 17:51:33 +0200

    Style:integration and css ok on Contact and LegalMentions. Footer fixed, scroll on main

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,9 +1,29 @@
 // Local import
 import './contact.css';
+import logoLinkedIn from '../../assets/linkedin-logo.png';
 
 const Contact = () => (
     <div className="contact">
-       page de contact
+        <h1 className="contact__h1">Contact</h1>
+        <p className="contact__presentation"> Bonjour,<br/>
+            Je suis Laëtitia, développeuse fullstack.<br/><br />
+            J'ai développé cette application pour mon usage personnel, mais si vous souhaitez l'utiliser, c'est avec plaisir!<br/><br/>
+            Vous pouvez consulter mon profil et mes réalisations en suivant le lien ci-dessous.
+        </p>
+        <a className="link_linkedIn" href="https://www.linkedin.com/in/laetitia-rodriguez/ ">
+            <img className="contact__logo_linkedIn" alt="" src={logoLinkedIn} />
+        </a>
+        <p>Vous pouvez aussi m'envoyer un message, je vous répondrai dans les meilleurs délais.</p>
+        <div className="contact__form-container">
+            <form className="contact__form">
+                <label>Votre email</label>
+                <input type="email"></input>
+                <label>Votre message</label>
+                <input type="text"/>
+                <label></label>
+                <input type="submit" value="Envoyer"/>
+            </form>
+        </div>
     </div>
 )
 
--- src/components/LegalMentions/index.js
+++ src/components/LegalMentions/index.js
@@ -3,7 +3,34 @@ import './legalMentions.css';
 
 const LegalMentions = () => (
     <div className="legal-mentions">
-        Page mentions légales
+        <h1 className="legal-mentions__h1">Mentions légales</h1>
+        <p className="legal-mentions__text">
+            Nom de l'application : EasyCook<br/><br/>
+            Hébergement : Amazon Web Services LLC, P.O. Box 81226 Seattle, WA 98108-1226 <br/><br/>
+            L’utilisation du présent site implique l’acceptation pleine et entière des
+            conditions générales d’utilisation décrites ci-après. Ces conditions d’utilisation
+            sont susceptibles d’être modifiées ou complétées à tout moment.<br/><br/>
+            Sauf mention contraire, tous les éléments accessibles sur le site (textes, images,
+            graphismes, logo, icônes, sons, logiciels, etc.) restent la propriété exclusive de
+            leurs auteurs, en ce qui concerne les droits de propriété intellectuelle ou
+            les droits d’usage.<br/><br/>
+            Le propriétaire du site décline toute responsabilité et n’est pas engagé
+            par le référencement via des liens hypertextes, de ressources tierces présentes
+            sur le réseau Internet, tant en ce qui concerne leur contenu que leur pertinence.<br/><br/>
+            Le propriétaire du site autorise les liens hypertextes vers l’une des pages de ce site,
+            à condition que ceux-ci ouvrent une nouvelle fenêtre et soient présentés de manière non
+            équivoque afin d’éviter :<br />
+            - tout risque de confusion entre le site citant et le propriétaire du site<br />
+            - ainsi que toute présentation tendancieuse, ou contraire aux lois en vigueur.<br />
+            Le propriétaire du site se réserve le droit de demander la suppression d’un lien
+            s’il estime que le site source ne respecte pas les règles ainsi définies.<br/><br/>
+            Tout utilisateur ayant créé un compte en fournissant emailet mot de passe peut accéder à tout moment à la modification
+            de ses données dans son compte en ligne. Pour la suppression, il peut la demander via le formulaire dans la page de contact.<br/><br/>
+            Un cookie d'authentification sera déposé sur le navigateur internet
+            pour tout utilisateur ayant créé un compte et s'y connectant, ceci dans
+            un but de sécurisation de la connexion au compte utilisateur. La durée de
+            validité de ce cookie est inférieure à 24h. Aucun cookie publicitaire n'est déposé.
+        </p>
     </div>
 )
 
